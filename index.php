fe2c0e0f30a5df714d910aac20dda3147eeb6141
urf <jehaby@ya.ru>
2014-10-02 20:23:03 +0400

    Rooting started

--- index.php
+++ index.php
@@ -2,9 +2,42 @@
 
 require_once 'includes.php';
 
-$model = new Model();
-$view = new View();
-$controller = new Controller();
+if (!empty($page = $_GET['page'])) {
+
+    $data = array(
+        'about' => array('model' => 'AboutModel', 'view' => 'AboutView', 'controller' => 'AboutController'),
+        'portfolio' => array('model' => 'PortfolioModel', 'view' => 'PortfolioView', 'controller' => 'PortfolioController')
+    );
+
+//    foreach($data as $key => $components){
+//        if ($page == $key) {
+//            $model = $components['model'];
+//            $view = $components['view'];
+//            $controller = $components['controller'];
+//            break;
+//        }
+//    }
+
+    echo "WWWWW!!!!!" . $page;
+
+    if (array_key_exists($page, $data)) {
+        $components = $data[$page];
+        $model = $components['model'];
+        $view = $components['view'];
+        $controller = $components['controller'];
+    }
+
+    if (isset($model)) {
+        $m = new $model();
+        $c = new $controller($model);
+        $v = new $view($model);
+        echo $v->output();
+    }
+} else {
+    $model = new Model();
+    $view = new View();
+    $controller = new Controller();
+}
 
 echo $view->output();
 
